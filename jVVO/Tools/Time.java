c3f0294f0b349ea87c80a29fab43f2c6b20a2d74
Philipp Matthes <github@philippmatth.es>
2018-06-10 16:06:11 +0200

    Fix time being calculated invertedly

--- jVVO/Tools/Time.java
+++ jVVO/Tools/Time.java
@@ -3,31 +3,10 @@ package jVVO.Tools;
 import java.util.Date;
 
 public final class Time {
-    public static int hoursUntil(Date date) {
-        if (date == null) { return 0; }
-
-        long diff = new Date().getTime() - date.getTime();
-        return (int) (diff / 3600000);
-    }
-
     public static int minutesUntil(Date date) {
         if (date == null) { return 0; }
 
-        long diff = new Date().getTime() - date.getTime();
+        long diff = date.getTime() - new Date().getTime();
         return (int) (diff / 60000);
-    }
-
-    public static int secondsUntil(Date date) {
-        if (date == null) { return 0; }
-
-        long diff = new Date().getTime() - date.getTime();
-        return (int) (diff / 1000);
-    }
-
-    public static int millisUntil(Date date) {
-        if (date == null) { return 0; }
-
-        long diff = new Date().getTime() - date.getTime();
-        return (int) diff;
     }
 }
